5d3ea0c4e77a9c78c76fc39cbf58bb2e64a2648d
ifurusato <ichiro.furusato@gmail.com>
2024-08-18 12:12:18 +1200

    removed unnecessary files

--- upy/colorama/README.txt
+++
@@ -1,5 +0,0 @@
-This is a substitute MicroPython library for Colorama by Jonathan Hartley.
-
-It offers a stub service that always returns empty strings, as MicroPython's
-implementation of sys.stdout does not support reassignment.
-
--- upy/colorama/__init__.py
+++
@@ -1,8 +0,0 @@
-# This module is part of a substitute library for Colorama by Jonathan Hartley,
-# and does nothing except return empty strings, since stdout in MicroPython is
-# not implemented as it is in CPython.
-
-from .initialise import init, deinit, reinit, colorama_text
-from .ansi import Fore, Back, Style, Cursor
-
-__version__ = '0.4.4'
